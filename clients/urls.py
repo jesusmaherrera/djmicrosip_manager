453ff61ea8869a582bf9fe8973458359c40c1764
Jesús Herrera <jesusmaherrera@gmail.com>
2015-04-15 10:26:40 -0600

    CHANGE URLS

--- clients/urls.py
+++ clients/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from clients import views
 
 urlpatterns = [
-	url(r'^getmykey/(?P<username>\w+)/(?P<computername>\w+)/(?P<macaddress>\w+)/$', views.GetMyKey.as_view()),
+	url(r'^getmykey/(?P<username>[a-zA-Z0-9.-]+)/(?P<computername>[a-zA-Z0-9.-]+)/(?P<macaddress>\w+)/$', views.GetMyKey.as_view()),
 ]
